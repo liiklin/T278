b664e182c60f3a8d549b0610abfaa72b5c865507
linjun <117280858@qq.com>
2016-06-06 22:27:46 +0800

    添加register方法

--- src/home/service/auth.js
+++ src/home/service/auth.js
@@ -4,111 +4,111 @@ import fetch from 'node-fetch';
 var logger = require('tracer').colorConsole();
 
 const baseUrl = 'http://202.115.80.69:8000',
-	version = 'v1';
+    version = 'v1';
 
 export default class extends think.service.base {
-	init(...args) {
-		super.init(...args);
-	}
+    init(...args) {
+        super.init(...args);
+    }
 
-	async auth(account, password, ip) {
-		// logger.info(account, password, ip);
-		let url = `${baseUrl}/${version}/auth`;
-		return new Promise((resolve, reject) => {
-			fetch(url, {
-					method: "POST",
-					headers: {
-					  "Content-Type": "application/x-www-form-urlencoded"
-					},
-					body: `username=${account}&password=${password}&userip=${ip}`
-				})
-				.then((response) => {
-					if (response.status >= 400) {
-						throw new Error("Bad response from server");
-						return reject("Bad response from server");
-					}
-					return response.text();
-				})
-				.then((status) => {
-					logger.info(status);
-					return resolve(status);
-				})
-				.catch((e)=>{
-					logger.error(e);
-					return reject(e);
-				});
-		})
-	}
+    async auth(account, password, ip) {
+        // logger.info(account, password, ip);
+        let url = `${baseUrl}/${version}/auth`;
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: `username=${account}&password=${password}&userip=${ip}`
+                })
+                .then((response) => {
+                    if (response.status >= 400) {
+                        throw new Error("Bad response from server");
+                        return reject("Bad response from server");
+                    }
+                    return response.text();
+                })
+                .then((status) => {
+                    logger.info(status);
+                    return resolve(status);
+                })
+                .catch((e) => {
+                    logger.error(e);
+                    return reject(e);
+                });
+        })
+    }
 
-	async status() {
-		let url = `${baseUrl}/${version}/status`
-		return new Promise((resolve, reject) => {
-			fetch(url)
-				.then((response) => {
-					if (response.status >= 400) {
-						throw new Error("Bad response from server");
-						return reject("Bad response from server");
-					}
-					return response.text();
-				})
-				.then((status) => {
-					logger.info(status);
-					return resolve(status);
-				})
-				.catch((e)=>{
-					logger.error(e);
-					return reject(e);
-				});
-		})
-	}
+    async status() {
+        let url = `${baseUrl}/${version}/status`
+        return new Promise((resolve, reject) => {
+            fetch(url)
+                .then((response) => {
+                    if (response.status >= 400) {
+                        throw new Error("Bad response from server");
+                        return reject("Bad response from server");
+                    }
+                    return response.text();
+                })
+                .then((status) => {
+                    logger.info(status);
+                    return resolve(status);
+                })
+                .catch((e) => {
+                    logger.error(e);
+                    return reject(e);
+                });
+        })
+    }
 
-	async logout(userip) {
-		let url = `${baseUrl}/${version}/logout`;
-		return new Promise((resolve, reject) => {
-			fetch(url, {
-					method: "POST",
-					headers: {
-					  "Content-Type": "application/x-www-form-urlencoded"
-					},
-					body: `userip=${userip}`
-				})
-				.then((response) => {
-					if (response.status >= 400) {
-						throw new Error("Bad response from server");
-						return reject("Bad response from server");
-					}
-					return response.text();
-				})
-				.then((status) => {
-					logger.info(status);
-					return resolve(status);
-				})
-				.catch((e)=>{
-					logger.error(e);
-					return reject(e);
-				});
-		})
-	}
+    async logout(userip) {
+        let url = `${baseUrl}/${version}/logout`;
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: `userip=${userip}`
+                })
+                .then((response) => {
+                    if (response.status >= 400) {
+                        throw new Error("Bad response from server");
+                        return reject("Bad response from server");
+                    }
+                    return response.text();
+                })
+                .then((status) => {
+                    logger.info(status);
+                    return resolve(status);
+                })
+                .catch((e) => {
+                    logger.error(e);
+                    return reject(e);
+                });
+        })
+    }
 
-	async ip() {
-		let url = `${baseUrl}/${version}/ip`;
-		return new Promise((resolve, reject) => {
-			fetch(url)
-				.then((response) => {
-					if (response.status >= 400) {
-						throw new Error("Bad response from server");
-						return reject("Bad response from server");
-					}
-					return response.text();
-				})
-				.then((ip) => {
-					logger.info(ip);
-					return resolve(ip);
-				})
-				.catch((e)=>{
-					logger.error(e);
-					return reject(e);
-				});
-		});
-	}
+    async ip() {
+        let url = `${baseUrl}/${version}/ip`;
+        return new Promise((resolve, reject) => {
+            fetch(url)
+                .then((response) => {
+                    if (response.status >= 400) {
+                        throw new Error("Bad response from server");
+                        return reject("Bad response from server");
+                    }
+                    return response.text();
+                })
+                .then((ip) => {
+                    logger.info(ip);
+                    return resolve(ip);
+                })
+                .catch((e) => {
+                    logger.error(e);
+                    return reject(e);
+                });
+        });
+    }
 }
---
+++ src/home/service/register.js
@@ -0,0 +1,180 @@
+'use strict';
+
+import fetch from 'node-fetch';
+var logger = require('tracer').colorConsole();
+
+const baseUrl = 'http://202.115.80.69:8000',
+    version = 'v1';
+
+export default class extends think.service.base {
+    init(...args) {
+        super.init(...args);
+    }
+    async idchecker(cardid, password) {
+        let url = `${baseUrl}/${version}/id_checker`;
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: `cardid=${cardid}&password=${password}`
+                })
+                .then((response) => {
+                    if (response.status >= 400) {
+                        throw new Error("Bad response from server");
+                        return reject("Bad response from server");
+                    }
+                    return response.text();
+                })
+                .then((status) => {
+                    logger.info(status);
+                    return resolve(status);
+                })
+                .catch((e) => {
+                    logger.error(e);
+                    return reject(e);
+                });
+        });
+    }
+
+    async sendverifycode(phone) {
+        let url = `${baseUrl}/${version}/send_verifycode`;
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: `phone=${phone}`
+                })
+                .then((response) => {
+                    if (response.status >= 400) {
+                        throw new Error("Bad response from server");
+                        return reject("Bad response from server");
+                    }
+                    return response.text();
+                })
+                .then((status) => {
+                    logger.info(status);
+                    return resolve(status);
+                })
+                .catch((e) => {
+                    logger.error(e);
+                    return reject(e);
+                });
+        });
+    }
+
+    async phonechecker(phone, verifycode) {
+        let url = `${baseUrl}/${version}/phone_checker`;
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: `phone=${phone}&verifycode=${verifycode}`
+                })
+                .then((response) => {
+                    if (response.status >= 400) {
+                        throw new Error("Bad response from server");
+                        return reject("Bad response from server");
+                    }
+                    return response.text();
+                })
+                .then((status) => {
+                    logger.info(status);
+                    return resolve(status);
+                })
+                .catch((e) => {
+                    logger.error(e);
+                    return reject(e);
+                });
+        });
+    }
+
+    async register(cardid, phone, password) {
+        let url = `${baseUrl}/${version}/register`;
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: `cardid=${cardid}&phone=${phone}&password=${password}`
+                })
+                .then((response) => {
+                    if (response.status >= 400) {
+                        throw new Error("Bad response from server");
+                        return reject("Bad response from server");
+                    }
+                    return response.text();
+                })
+                .then((status) => {
+                    logger.info(status);
+                    return resolve(status);
+                })
+                .catch((e) => {
+                    logger.error(e);
+                    return reject(e);
+                });
+        });
+    }
+
+    async unregister(cardid) {
+        let url = `${baseUrl}/${version}/unregister`;
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: `cardid=${cardid}`
+                })
+                .then((response) => {
+                    if (response.status >= 400) {
+                        throw new Error("Bad response from server");
+                        return reject("Bad response from server");
+                    }
+                    return response.text();
+                })
+                .then((status) => {
+                    logger.info(status);
+                    return resolve(status);
+                })
+                .catch((e) => {
+                    logger.error(e);
+                    return reject(e);
+                });
+        });
+    }
+
+    async modifypassword(cardid, password) {
+        let url = `${baseUrl}/${version}/modify_password`;
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: `cardid=${cardid}&password=${password}`
+                })
+                .then((response) => {
+                    if (response.status >= 400) {
+                        throw new Error("Bad response from server");
+                        return reject("Bad response from server");
+                    }
+                    return response.text();
+                })
+                .then((status) => {
+                    logger.info(status);
+                    return resolve(status);
+                })
+                .catch((e) => {
+                    logger.error(e);
+                    return reject(e);
+                });
+        });
+    }
+}
